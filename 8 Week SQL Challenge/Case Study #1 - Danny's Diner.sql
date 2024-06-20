69ec0551d87c34651e8eedd55d3279e8a45d349c
Truong Hoai Nam <85633982+Kuunna@users.noreply.github.com>
2024-06-20 12:08:57 +0300

    Update Case Study #1 - Danny's Diner.sql

--- 8 Week SQL Challenge/Case Study #1 - Danny's Diner.sql
+++ 8 Week SQL Challenge/Case Study #1 - Danny's Diner.sql
@@ -60,7 +60,7 @@ ORDER BY s.customer_id
 -- 2. How many days has each customer visited the restaurant
 SELECT customer_id, COUNT(order_date) AS visited_date
 FROM (
-	SELECT customer_id, order_date
+    SELECT customer_id, order_date
     FROM sales
     GROUP BY customer_id, order_date
 ) unique_dates
@@ -96,9 +96,9 @@ LIMIT 1
 -- WITH customer_purchases AS (
     SELECT 
         s.customer_id, 
-  		m.product_name,
-  		COUNT(s.product_id) AS purchase_count,
-  		ROW_NUMBER() OVER (
+  	m.product_name,
+  	COUNT(s.product_id) AS purchase_count,
+  	ROW_NUMBER() OVER (
           PARTITION BY s.customer_id
           ORDER BY COUNT(s.product_id) DESC) AS rk
     FROM sales s
@@ -121,8 +121,8 @@ WITH first_purchases AS (
       ) AS rank
     FROM sales s
     JOIN menu m ON s.product_id = m.product_id
-  	JOIN members mb ON s.customer_id = mb.customer_id
-					AND s.order_date  mb.join_date
+    JOIN members mb ON s.customer_id = mb.customer_id
+		    AND s.order_date  mb.join_date
 )
 SELECT customer_id, product_name
 FROM first_purchases
@@ -140,8 +140,8 @@ WITH first_purchases AS (
       ) AS rank
     FROM sales s
     JOIN menu m ON s.product_id = m.product_id
-  	JOIN members mb ON s.customer_id = mb.customer_id
-					AND s.order_date  mb.join_date
+    JOIN members mb ON s.customer_id = mb.customer_id
+		    AND s.order_date < mb.join_date
 )
 SELECT customer_id, product_name
 FROM first_purchases
@@ -150,13 +150,13 @@ WHERE rank = 1;
 
 -- 8. What is the total items and amount spent for each member before they became a member
 SELECT 
-	s.customer_id,
+    s.customer_id,
     COUNT(s.product_id) AS total_items,
     SUM(m.price) AS amount_spent
 FROM sales s
 JOIN menu m ON s.product_id = m.product_id
 JOIN members mb ON s.customer_id = mb.customer_id
-  				AND s.order_date  mb.join_date
+  		AND s.order_date < mb.join_date
 GROUP BY s.customer_id
 ORDER BY s.customer_id
 
@@ -174,7 +174,7 @@ WITH customer_spending AS (
   where s.order_date BETWEEN '2021-01-01' AND '2021-01-31'
 )
 SELECT customer_id,
-	   SUM(points) AS total_points
+       SUM(points) AS total_points
 FROM customer_spending
 GROUP BY customer_id
 ORDER BY customer_id
@@ -187,13 +187,13 @@ WITH customer_spending AS (
     s.customer_id,
     s.product_id,
     CASE
-		-- 2x points if within the first week of joining for all items
+	-- 2x points if within the first week of joining for all items
     	WHEN s.order_date BETWEEN mb.join_date 
-  						  AND DATEADD(day, 6, mb.join_date) 
+  			  AND DATEADD(day, 6, mb.join_date) 
                           THEN (m.price  20)
         -- 2x points if sushi, outside of the first week of joining
-  		WHEN m.product_id = 1 THEN (m.price  20)
-  		ELSE (m.price  10)
+  	WHEN m.product_id = 1 THEN (m.price  20)
+  	ELSE (m.price  10)
     END AS points
   FROM sales s
   JOIN menu m ON s.product_id = m.product_id
@@ -202,7 +202,7 @@ WITH customer_spending AS (
   	AND s.order_date BETWEEN '2021-01-01' AND '2021-01-31'
 )
 SELECT customer_id,
-	   SUM(points) AS total_points
+       SUM(points) AS total_points
 FROM customer_spending
 GROUP BY customer_id
 ORDER BY customer_id
