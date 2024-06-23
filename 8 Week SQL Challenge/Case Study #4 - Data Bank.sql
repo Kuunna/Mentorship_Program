ca551ef721cfc6946fcef9e6411e6dfde7eb5ce5
Truong Hoai Nam <85633982+Kuunna@users.noreply.github.com>
2024-06-23 16:38:01 +0300

    Case Study #4 - Data Bank.sql

--- 8 Week SQL Challenge/Case Study #4 - Data Bank.sql
+++ 8 Week SQL Challenge/Case Study #4 - Data Bank.sql
@@ -32,3 +32,44 @@ GROUP BY cn.region_id, r.region_name
 ORDER by cn.region_id
 
 -- 4. How many days on average are customers reallocated to a different node? 
+WITH node_days AS (
+  SELECT 
+    customer_id, 
+    node_id,
+    end_date - start_date AS days_in_node
+  FROM customer_nodes
+  WHERE end_date != '9999-12-31'
+) , total_node_days AS (
+  SELECT 
+    customer_id,
+    node_id,
+    SUM(days_in_node) AS total_days_in_node
+  FROM node_days
+  GROUP BY customer_id, node_id
+)
+SELECT ROUND(AVG(total_days_in_node), 1) AS avg_node_reallocation_days
+FROM total_node_days
+
+-- 5. What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
+ WITH node_days AS (
+  SELECT 
+    customer_id, 
+    region_id,
+    end_date - start_date AS days_in_node
+  FROM customer_nodes
+  WHERE end_date != '9999-12-31'
+), total_node_days AS (
+  SELECT 
+    customer_id,
+    region_id,
+    SUM(days_in_node) AS total_days_in_node
+  FROM node_days
+  GROUP BY customer_id, region_id
+)
+SELECT 
+  region_id,
+  PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY total_days_in_node) AS median_days,
+  PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY total_days_in_node) AS percentile_80,
+  PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY total_days_in_node) AS percentile_95
+FROM total_node_days
+GROUP BY region_id
