c34b1019fffc78514d45cb98c935def87996b929
Truong Hoai Nam <85633982+Kuunna@users.noreply.github.com>
2024-06-29 12:45:40 +0300

    Case Study #7 - Balanced Tree Clothing.sql

--- 8 Week SQL Challenge/Case Study #7 - Balanced Tree Clothing.sql
+++ 8 Week SQL Challenge/Case Study #7 - Balanced Tree Clothing.sql
@@ -135,22 +135,76 @@ SELECT
 from top_selling
 WHERE ranking = 1
 -- 4. What is the total quantity, revenue and discount for each category? 
-
+SELECT 
+  p.category_id,
+  p.category_name, 
+  SUM(s.qty) AS total_quantity,
+  SUM(s.qty * s.price) AS total_revenue,
+  SUM(s.qty * s.price * s.discount / 100) AS total_discount
+FROM sales s
+INNER JOIN product_details p oN s.prod_id = p.product_id
+GROUP BY p.category_id, p.category_name
+ORDER BY p.category_id
 
 -- 5. What is the top selling product for each category? 
+WITH top_selling AS ( 
+  SELECT 
+    p.category_name, 
+    p.product_name,
+    SUM(s.qty) AS total_quantity,
+    DENSE_RANK() OVER (
+      PARTITION BY p.category_name
+      ORDER BY SUM(s.qty) DESC) AS ranking
+  FROM sales s
+  JOIN product_details p ON s.prod_id = p.product_id
+  GROUP BY p.category_name, p.product_name
+)
 
+SELECT 
+  category_name, 
+  product_name,
+  total_quantity
+FROM top_selling
+WHERE ranking = 1
 
 -- 6. What is the percentage split of revenue by product for each segment? 
-
+WITH revenue_by_product AS ( 
+  SELECT
+    prod_id,
+  	ROUND(100.0 * SUM(price * qty) / (SELECT SUM(price * qty) FROM sales), 2) revenue_percent_by_product
+  FROM sales
+  GROUP by prod_id
+)
+SELECT 
+  pd.segment_name,
+  pd.product_name, 
+  r.revenue_percent_by_product
+FROM revenue_by_product r
+JOIN product_details pd ON r.prod_id = pd.product_id
 
 -- 7. What is the percentage split of revenue by segment for each category? 
-
+SELECT
+    pd.category_name,
+  	pd.segment_name,
+	ROUND(100.0 * SUM(s.price * s.qty) / (SELECT SUM(price * qty) FROM sales), 2) revenue_percent_by_segment
+FROM sales s
+join product_details pd on s.prod_id = pd.product_id
+GROUP by pd.category_name, pd.segment_name
 
 -- 8. What is the percentage split of total revenue by category? 
-
+SELECT
+	pd.category_id,
+    pd.category_name,
+	ROUND(100.0 * SUM(s.price * s.qty) / (SELECT SUM(price * qty) FROM sales), 2) revenue_percent_by_category
+FROM sales s
+join product_details pd on s.prod_id = pd.product_id
+GROUP by pd.category_id,pd.category_name
 
 -- 9. What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions) 
-
-
--- 10. What is the most common combination of at least 1 quantity of any 3 products in a 1 single transaction?
-
+SELECT
+    pd.product_name,
+	ROUND(100.0 * COUNT(DISTINCT s.txn_id) / (SELECT count(DISTINCT txn_id) FROM sales), 2) revenue_percent_by_segment
+FROM sales s
+join product_details pd on s.prod_id = pd.product_id
+WHERE s.qty > 0
+GROUP by pd.product_name
