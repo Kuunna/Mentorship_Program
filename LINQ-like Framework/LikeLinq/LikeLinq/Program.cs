c107aefe0328e84693b731561e860e1df821c644
Truong Hoai Nam <truonghoainam2k2@gmail.com>
2024-09-14 13:39:46 +0300

    update 14.09.2024

--- LINQ-like Framework/LikeLinq/LikeLinq/Program.cs
+++ LINQ-like Framework/LikeLinq/LikeLinq/Program.cs
@@ -6,5 +6,26 @@ using System.Linq.Expressions;
 
 namespace LikeLinq
 {
+    public interface IMyQueryable<T>
+    {
+        Expression Expression { get; }
+        IMyQueryProvider Provider { get; }
+    }
+    public interface IMyQueryProvider
+    {
+        IQueryable<T> CreateQuery<T>(Expression expression);
+        object Execute(Expression expression);
+    }
+    public class MyQueryable<T> : IMyQueryable<T>
+    {
+        public Expression Expression { get; private set; }
+        public IMyQueryProvider Provider { get; private set; }
+
+        public MyQueryable(IMyQueryProvider provider, Expression expression)
+        {
+            Provider = provider;
+            Expression = expression;
+        }
+    }
 
 }
