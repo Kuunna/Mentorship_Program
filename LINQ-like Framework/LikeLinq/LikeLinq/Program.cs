2a9d1187bcb93af06558384d4185a94e78d51786
Truong Hoai Nam <truonghoainam2k2@gmail.com>
2024-09-17 00:01:14 +0300

    Product 17.09.2024

--- LINQ-like Framework/LikeLinq/LikeLinq/Program.cs
+++ LINQ-like Framework/LikeLinq/LikeLinq/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 
 namespace LikeLinq
@@ -13,8 +11,24 @@ namespace LikeLinq
 
         IQueryable<T> Where(Expression<Func<T, bool>> predicate);
         IQueryable<T> Select<TResult>(Expression<Func<T, TResult>> selector);
+        IQueryable<T> OrderBy(Expression<Func<T, object>> keySelector);
+        IQueryable<T> OrderByDescending(Expression<Func<T, object>> keySelector);
+        IQueryable<T> GroupBy(Expression<Func<T, object>> keySelector);
+        IQueryable<TResult> GroupBy<TKey, TResult>(Expression<Func<T, TKey>> keySelector,
+                                                   Expression<Func<TKey, IEnumerable<T>, TResult>> elementSelector);
+        IQueryable<TResult> Join<TOuter, TInner, TKey, TResult>(IQueryable<TOuter> outer,
+                                                             IQueryable<TInner> inner,
+                                                             Expression<Func<T, TKey>> outerKeySelector,
+                                                             Expression<Func<TOuter, TKey>> innerKeySelector,
+                                                             Expression<Func<T, TOuter, TInner, TResult>> resultSelector);
+        IQueryable<T> Take(int count);
+        IQueryable<T> Skip(int count);
+        int Count();
+        bool Any();
+        bool All(Expression<Func<T, bool>> predicate);
         // ... các phương thức khác
     }
+
     public interface IQueryProvider
     {
         IQueryable<T> CreateQuery<T>(Expression expression);
