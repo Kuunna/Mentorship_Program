2cb8ccce9f5dfc880192255cac59c4ed6c285e53
Truong Hoai Nam <truonghoainam2k2@gmail.com>
2024-09-30 00:23:37 +0300

    Last update 29.09.2024

--- Daily_dev Challenge/Daily_dev/daily_dev/Controllers/newsController.cs
+++ Daily_dev Challenge/Daily_dev/daily_dev/Controllers/newsController.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace daily_dev.Controllers
 {
     [Route("api/[controller]")]
@@ -17,29 +15,88 @@ namespace daily_dev.Controllers
             _context = context;
         }
 
+        // GET: api/news
         [HttpGet]
-        public async Task<IActionResult> GetNews()
+        public async Task<ActionResult<IEnumerable<Fact_News>>> GetAllNews()
         {
-            var newsList = await _context.Fact_News.ToListAsync();
-            return Ok(newsList);
+            return await _context.Fact_News.ToListAsync();
         }
 
-        [HttpGet("{newsId}")]
-        public async Task<IActionResult> GetNewsById(int newsId)
+        // GET: api/news/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fact_News>> GetNewsById(int id)
         {
-            var news = await _context.Fact_News.FindAsync(newsId);
-            if (news == null)
+            var newsItem = await _context.Fact_News.FindAsync(id);
+
+            if (newsItem == null)
+            {
                 return NotFound();
-            return Ok(news);
+            }
+
+            return newsItem;
         }
 
+        // POST: api/news
         [HttpPost]
-        public async Task<IActionResult> AddNews(Fact_News news)
+        public async Task<ActionResult<Fact_News>> CreateNews([FromBody] Fact_News news)
         {
+            if (news == null)
+            {
+                return BadRequest("News data is null");
+            }
+
             _context.Fact_News.Add(news);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetNewsById), new { newsId = news.NewID }, news);
+
+            return CreatedAtAction(nameof(GetNewsById), new { id = news.NewID }, news);
+        }
+
+        // PUT: api/news/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateNews(int id, [FromBody] Fact_News news)
+        {
+            if (id != news.NewID)
+            {
+                return BadRequest("News ID mismatch");
+            }
+
+            var existingNews = await _context.Fact_News.FindAsync(id);
+            if (existingNews == null)
+            {
+                return NotFound();
+            }
+
+            existingNews.Title = news.Title;
+            existingNews.Content = news.Content;
+            existingNews.PublishedDate = news.PublishedDate;
+            existingNews.SourceID = news.SourceID;
+            existingNews.TopicID = news.TopicID;
+            existingNews.Author = news.Author;
+            existingNews.ImageURL = news.ImageURL;
+            existingNews.ViewCount = news.ViewCount;
+            existingNews.LikeCount = news.LikeCount;
+            existingNews.CommentCount = news.CommentCount;
+
+            _context.Fact_News.Update(existingNews);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/news/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNews(int id)
+        {
+            var newsItem = await _context.Fact_News.FindAsync(id);
+            if (newsItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fact_News.Remove(newsItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
-
 }
