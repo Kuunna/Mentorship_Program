df7a6e3b348c2ea208d5dd21012b00844ae4c090
Truong Hoai Nam <85633982+Kuunna@users.noreply.github.com>
2024-09-30 21:03:57 +0300

    Delete Daily_dev Challenge directory

--- Daily_dev Challenge/Daily_dev/daily_dev/Models/User_Source.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace daily_dev.Models
-{
-    public class User_Source
-    {
-        public int UserID { get; set; }  // Referencing Dim_User.UserID
-        public int SourceID { get; set; }  // Referencing Dim_Source.SourceID
-        public DateTime FollowDate { get; set; }
-    }
-}
