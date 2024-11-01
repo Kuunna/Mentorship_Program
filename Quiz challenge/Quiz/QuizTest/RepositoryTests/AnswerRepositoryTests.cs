9e9f7584317f91bfa0d5b9606083166325b97f75
Kunna <truonghoainam2k2@gmail.com>
2024-11-02 02:13:41 +0300

    Add AnswerRepositoryTests and QuizRepositoryTests

---
+++ Quiz challenge/Quiz/QuizTest/RepositoryTests/AnswerRepositoryTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuizChallenge.Models;
+using QuizChallenge.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace QuizTest.RepositoryTests
+{
+    [TestClass]
+    public class AnswerRepositoryTests
+    {
+        private readonly string _connectionString = "Data Source=HOAINAM;Initial Catalog=Quiz;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+        private AnswerRepository _answerRepository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _answerRepository = new AnswerRepository(_connectionString);
+        }
+
+        [TestMethod]
+        public void TestAddAnswer()
+        {
+            // Arrange
+            var answer = new Answer
+            {
+                AnswerText = "This is a test Add answer.",
+                IsCorrect = true,
+                IsDynamic = false,
+                CanBeSuggested = false
+            };
+
+            // Act
+            _answerRepository.AddAnswer(answer);
+
+            // Assert
+            var addedAnswer = _answerRepository.GetAllAnswer().FirstOrDefault(a => a.AnswerText == "This is a test Add answer.");
+
+            Assert.IsNotNull(addedAnswer, "Answer should be added successfully.");
+            Assert.AreEqual(answer.AnswerText, addedAnswer.AnswerText);
+            Assert.AreEqual(answer.IsCorrect, addedAnswer.IsCorrect);
+            Assert.AreEqual(answer.IsDynamic, addedAnswer.IsDynamic);
+            Assert.AreEqual(answer.CanBeSuggested, addedAnswer.CanBeSuggested);
+        }
+
+        [TestMethod]
+        public void TestGetAnswerById()
+        {
+            // Arrange
+            var answer = new Answer
+            {
+                AnswerText = "This is a test Get answer.",
+                IsCorrect = true,
+                IsDynamic = false,
+                CanBeSuggested = false
+            };
+            _answerRepository.AddAnswer(answer);
+
+            // Act
+            var addedAnswer = _answerRepository.GetAllAnswer().FirstOrDefault(a => a.AnswerText == "This is a test Get answer.");
+
+            // Assert
+            Assert.IsNotNull(addedAnswer, "Answer should be retrieved successfully.");
+            Assert.AreEqual(answer.AnswerText, addedAnswer.AnswerText);
+        }
+
+        [TestMethod]
+        public void TestUpdateAnswer()
+        {
+            // Arrange
+            var answer = new Answer
+            {
+                AnswerText = "This is a test update answer.",
+                IsCorrect = true,
+                IsDynamic = false,
+                CanBeSuggested = false
+            };
+            _answerRepository.AddAnswer(answer);
+
+            // Act
+            var addedAnswer = _answerRepository.GetAllAnswer().FirstOrDefault(a => a.AnswerText == "This is a test update answer.");
+            addedAnswer.AnswerText = "Updated answer text.";
+            addedAnswer.IsCorrect = false;
+            _answerRepository.UpdateAnswer(addedAnswer);
+
+            // Assert
+            var updatedAnswer = _answerRepository.GetAnswerById(addedAnswer.Id);
+            Assert.AreEqual("Updated answer text.", updatedAnswer.AnswerText);
+            Assert.IsFalse(updatedAnswer.IsCorrect);
+        }
+
+        [TestMethod]
+        public void TestDeleteAnswer()
+        {
+            // Arrange
+            var answer = new Answer
+            {
+                AnswerText = "This is a test Delete answer.",
+                IsCorrect = true,
+                IsDynamic = false,
+                CanBeSuggested = false
+            };
+            _answerRepository.AddAnswer(answer);
+
+            // Act
+            var addedAnswer = _answerRepository.GetAllAnswer().FirstOrDefault(a => a.AnswerText == "This is a test Delete answer.");
+            _answerRepository.DeleteAnswer(addedAnswer.Id);
+
+            // Assert
+            var deletedAnswer = _answerRepository.GetAnswerById(addedAnswer.Id);
+            Assert.IsNull(deletedAnswer, "Answer should be deleted successfully.");
+        }
+
+        [TestMethod]
+        public void TestGetAnswersByQuestionId()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestGetCorrectAnswersForQuestion()
+        {
+            
+        }
+    }
+}
---
+++ Quiz challenge/Quiz/QuizTest/RepositoryTests/QuizRepositoryTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuizChallenge.Models;
+using QuizChallenge.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace QuizTest.RepositoryTests
+{
+    [TestClass]
+    public class QuizRepositoryTests
+    {
+        private QuizRepository _quizRepository;
+        private const string TestConnectionString = "Data Source=HOAINAM;Initial Catalog=Quiz;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;"; 
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _quizRepository = new QuizRepository(TestConnectionString);
+        }
+
+        [TestMethod]
+        public void TestAddQuiz()
+        {
+            var quiz = new Quiz
+            {
+                Title = "Test Quiz",
+                CreatedAt = DateTime.Now,
+                Description = "This is a test quiz",
+                TimeLimit = 30
+            };
+
+            _quizRepository.AddQuiz(quiz);
+
+            var addedQuiz = _quizRepository.GetAllQuizzes().FirstOrDefault(q => q.Title == "Test Quiz");
+            Assert.IsNotNull(addedQuiz);
+            Assert.AreEqual(quiz.Description.TrimEnd('.'), addedQuiz.Description.TrimEnd('.'));
+            Assert.AreEqual(quiz.TimeLimit, addedQuiz.TimeLimit);
+        }
+
+
+        [TestMethod]
+        public void TestGetQuizById()
+        {
+            // Arrange
+            var quiz = new Quiz
+            {
+                Title = "Another Test Quiz",
+                CreatedAt = DateTime.Now,
+                Description = "Another test description",
+                TimeLimit = 45
+            };
+            _quizRepository.AddQuiz(quiz);
+
+            // Act
+            var addedQuiz = _quizRepository.GetAllQuizzes().FirstOrDefault(q => q.Title == "Another Test Quiz");
+            var retrievedQuiz = _quizRepository.GetQuizById(addedQuiz.Id);
+
+            // Assert
+            Assert.IsNotNull(retrievedQuiz);
+            Assert.AreEqual(addedQuiz.Title, retrievedQuiz.Title);
+            Assert.AreEqual(addedQuiz.Description, retrievedQuiz.Description);
+        }
+
+        [TestMethod]
+        public void TestUpdateQuiz()
+        {
+            // Arrange
+            var quiz = new Quiz
+            {
+                Title = "Quiz to Update",
+                CreatedAt = DateTime.Now,
+                Description = "Update this quiz",
+                TimeLimit = 30
+            };
+            _quizRepository.AddQuiz(quiz);
+
+            var addedQuiz = _quizRepository.GetAllQuizzes().FirstOrDefault(q => q.Title == "Quiz to Update");
+            Assert.IsNotNull(addedQuiz, "Quiz should be added successfully.");
+
+            addedQuiz.Description = "Updated description";
+            addedQuiz.TimeLimit = 60;
+
+            // Act
+            _quizRepository.UpdateQuiz(addedQuiz);
+            var updatedQuiz = _quizRepository.GetQuizById(addedQuiz.Id);
+
+            // Assert
+            Assert.AreEqual("Updated description", updatedQuiz.Description);
+            Assert.AreEqual(60, updatedQuiz.TimeLimit);
+        }
+
+
+        [TestMethod]
+        public void TestDeleteQuiz()
+        {
+            // Arrange
+            var quiz = new Quiz
+            {
+                Title = "Quiz to Delete",
+                CreatedAt = DateTime.Now,
+                Description = "This quiz will be deleted",
+                TimeLimit = 30
+            };
+            _quizRepository.AddQuiz(quiz);
+            var addedQuiz = _quizRepository.GetAllQuizzes().FirstOrDefault(q => q.Title == "Quiz to Delete");
+
+            // Act
+            _quizRepository.DeleteQuiz(addedQuiz.Id);
+            var deletedQuiz = _quizRepository.GetQuizById(addedQuiz.Id);
+
+            // Assert
+            Assert.IsNull(deletedQuiz);
+        }
+    }
+}
